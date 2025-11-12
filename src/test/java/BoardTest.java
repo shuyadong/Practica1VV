d36c7a4c20b0dfd51739a48eb0ba602198b545b1
yueqixia <yueqi.xia@alumnos.upm.es>
2025-11-12 20:36:05 +0100

    Test Update

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -3,7 +3,10 @@ import main.Commons;
 import org.junit.jupiter.api.Test;
 import space_invaders.sprites.Alien;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
@@ -14,7 +17,7 @@ public class BoardTest {
     }
 
     @Test
-    public void test_GameInit(){
+    public void test_GameInit_Case1(){
         Alien alien;
         for (int i = 0; i < Commons.ALIEN_ROWS; i++) {
             for (int j = 0; j < Commons.ALIEN_COLUMNS; j++) {
@@ -22,6 +25,36 @@ public class BoardTest {
                 assertEquals((Commons.ALIEN_INIT_X) + j * (Commons.ALIEN_WIDTH + Commons.ALIEN_SEPARATOR), alien.getX());
                 assertEquals((Commons.ALIEN_INIT_Y) + i * (Commons.ALIEN_HEIGHT + Commons.ALIEN_SEPARATOR), alien.getY());
             }
+        }
+    }
+
+    @Test
+    public void test_Update_Case1() {
+        try{
+            board.setDeaths(24);
+
+            Method method = Board.class.getDeclaredMethod("update");
+            method.setAccessible(true);
+            method.invoke(board);
+            assertFalse(board.isInGame());
+
+        }catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test_Update_Caso2() {
+        try{
+            board.setDeaths(5);
+
+            Method method = Board.class.getDeclaredMethod("update");
+            method.setAccessible(true);
+            method.invoke(board);
+            assertTrue(board.isInGame());
+
+        } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
     }
 }
